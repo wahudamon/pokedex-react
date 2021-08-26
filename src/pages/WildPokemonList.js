c8bbf9576e23f998ca18091aab79f110fb8fa07b
wahudamon <afwa.bagas.1999@gmail.com>
2021-08-26 10:08:27 +0700

    feat: trying make card component using reactstrap

--- src/pages/WildPokemonList.js
+++ src/pages/WildPokemonList.js
@@ -1,6 +1,10 @@
 import '../App.css';
 import { useEffect, useState } from 'react';
 import { getAllPokemon } from '../utils/getPokemonList';
+import { 
+  Button, Card, CardText, CardBody,
+  CardTitle, 
+} from 'reactstrap';
 
 function WildPokemonList() {
   const [wildPokemon, getWildPokemon] = useState([]);
@@ -18,7 +22,13 @@ function WildPokemonList() {
     <div className="App">
       <header className="App-header">
         {wildPokemon.map(pokemon => (
-          <p>{pokemon.name}</p>
+          <Card>
+            <CardBody>
+              <CardTitle style={{color: '#000', fontWeight: 'bold'}}><h3>{pokemon.name}</h3></CardTitle>
+              <CardText style={{color: '#000', fontSize: '12pt'}}>This is pokemon.</CardText>
+              <Button color="danger">Catch!</Button>
+            </CardBody>
+          </Card>
         ))}
       </header>
     </div>
