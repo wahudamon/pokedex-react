b12d441b573821062abf95ddcf789dd28a1caba3
wahudamon <afwa.bagas.1999@gmail.com>
2021-08-26 10:36:20 +0700

    fix: change sizing on wild pokemon card component

--- src/pages/WildPokemonList.js
+++ src/pages/WildPokemonList.js
@@ -3,7 +3,9 @@ import { useEffect, useState } from 'react';
 import { getAllPokemon } from '../utils/getPokemonList';
 import { 
   Button, Card, CardText, CardBody,
-  CardTitle, 
+  CardTitle,
+  Row,
+  Col, 
 } from 'reactstrap';
 
 function WildPokemonList() {
@@ -19,18 +21,22 @@ function WildPokemonList() {
   }, []);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        {wildPokemon.map(pokemon => (
-          <Card>
-            <CardBody>
-              <CardTitle style={{color: '#000', fontWeight: 'bold'}}><h3>{pokemon.name}</h3></CardTitle>
-              <CardText style={{color: '#000', fontSize: '12pt'}}>This is pokemon.</CardText>
-              <Button color="danger">Catch!</Button>
-            </CardBody>
-          </Card>
-        ))}
-      </header>
+    <div>
+      <div className="App-header">
+        <Row className="mx-5 my-5 g-3">
+          {wildPokemon.map(pokemon => (
+            <Col sm="3" md="5" lg="3">
+              <Card body className="text-center">
+                <CardBody>
+                  <CardTitle className="text-capitalize" style={{color: '#000', fontWeight: 'bold'}}><h3>{pokemon.name}</h3></CardTitle>
+                  <CardText style={{color: '#000', fontSize: '12pt'}}>This is pokemon.</CardText>
+                  <Button color="danger">Catch!</Button>
+                </CardBody>
+              </Card>
+            </Col>
+          ))}
+          </Row>
+      </div>
     </div>
   )
 }
--- src/utils/getPokemonList.js
+++ src/utils/getPokemonList.js
@@ -1,5 +1,5 @@
 export const getAllPokemon = async () => {
-  const data = await fetching('https://pokeapi.co/api/v2/pokemon?limit=10&offset=10');
+  const data = await fetching('https://pokeapi.co/api/v2/pokemon?limit=100&offset=100');
   return data.results;
 }
 
