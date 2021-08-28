1effa701e2cca42bffb6840a929827a88ba7f21f
wahudamon <afwa.bagas.1999@gmail.com>
2021-08-28 09:44:49 +0700

    feat: create banner and image element in details page

--- src/pages/PokemonDetails/index.js
+++ src/pages/PokemonDetails/index.js
@@ -1,9 +1,30 @@
+import { Button, Card, CardBody, CardTitle, Col, Container, Row } from 'reactstrap';
 import '../../App.css';
+import { PokemonColors } from '../../components/PokemonColors';
 
 export default function PokemonDetails() {
   return(
-    <div>
-      Hello World!
+    <div className="app">
+      <div style={{backgroundColor: PokemonColors.grass}} className="details-header mt-3">
+        <img width="200px" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/38.png" alt="ninetales" />
+      </div>
+      <Container>
+        <Row>
+          <Col>
+            <Card body className="text-center" style={{border: "0"}}>
+              <CardBody>
+                <CardTitle className="text-capitalize" style={{color: "#000", fontSize: "18pt", fontWeight: "bold"}}>ninetales</CardTitle>
+              </CardBody>
+            </Card>
+          </Col>
+        </Row>
+        <Row>
+          <Col sm="2" md="5" lg="3">
+            <Button style={{width: '80px', backgroundColor: PokemonColors.grass, border: 0}}>Grass</Button>
+            <Button style={{width: '80px', backgroundColor: PokemonColors.fire, border: 0}}>Fire</Button>
+          </Col>
+        </Row>
+      </Container>
     </div>
   )
 }
