94be63f42369dc53cb676a5185129ab8b7f79883
wahudamon <afwa.bagas.1999@gmail.com>
2021-09-04 16:23:40 +0700

    feat: create a method to catch pokemon and store it in local storage

--- src/components/CatchingModal/index.js
+++ src/components/CatchingModal/index.js
@@ -1,6 +1,87 @@
+import { useEffect, useState } from "react";
 import { Modal, ModalHeader, ModalBody, ModalFooter, Button } from "reactstrap";
+import { useAddMyPokemonList, useMyPokemonList } from "../../context/context";
 
-function CatchingModal(props) {
+const generateCaughtPokemon = (nickName, pokemonData) => {
+  const caughtPokemon = {
+    nickname: nickName,
+    name: pokemonData.name,
+    image: pokemonData.sprites.front_default,
+    type: pokemonData.types.map(type => (type.type.name))
+  }
+  return caughtPokemon;
+}
+
+export default function CatchingModal(props, { data }) {
+  const [catching, setCatching] = useState(false);
+  const [firstTry, setFirstTry] = useState(true);
+  const [success, setSuccess] = useState(false);
+  const [nickName, setNickname] = useState("");
+  const [nickNameError, setNicknameError] = useState(false);
+  const [nickNameMsg, setNicknameMsg] = useState("");
+  const [nickNameExist, setNicknameExist] = useState(false);
+  const handleChange = (event) => setNickname(event.target.value);
+
+  const myPokemonList = useMyPokemonList();
+  const addMyPokemon = useAddMyPokemonList();
+
+  const getRandomItem = () => {
+    const arr = [1, 0];
+    const randomIndex = Math.floor(Math.random() * arr.length);
+    const item = arr[randomIndex];
+    return item;
+  }
+
+  const catchingPokemon = async () => {
+    setCatching(true);
+    setTimeout(() => {
+      setCatching(false);
+      setFirstTry(false);
+      setSuccess(!!getRandomItem())
+    }, 3000)
+  }
+
+  // const { isOpen, onOpen, onClose } = useDisclosure(); ini punya chakra ui
+
+  const setDefault = () => {
+    setFirstTry(true);
+    setSuccess(false);
+    setNickname("");
+    setNicknameError(false);
+    setNicknameMsg("");
+  }
+
+  const closeCatching = () => {
+    //onClose(); punya chakra ui
+    setDefault();
+  }
+
+  const setFalseNickName = () => {
+    setNicknameError(false);
+  }
+
+  const savePokemon = () => {
+    if(nickName === "") {
+      setNicknameError(true);
+      setNicknameMsg("Nickname harus diisi!");
+    } else if(nickNameExist) {
+      setNicknameError(true);
+      setNicknameMsg("Nickname harus unik!");
+    } else {
+      setDefault();
+      const pokemonCaught = generateCaughtPokemon(nickName, data);
+      addMyPokemon(pokemonCaught);
+      console.log(pokemonCaught);
+      //onClose(); punya chakra ui
+      //saveToast(nickName); punya chakra ui
+    }
+  }
+
+  useEffect(() => {
+    localStorage.setItem('myPokemon', JSON.stringify(myPokemonList));
+    setNicknameExist(myPokemonList.some(el => el.nickName === nickName));
+  }, [myPokemonList, nickName]);
+
   return (
     <Modal isOpen={props.visible} toggle={props.onSetVisibility}>
       <ModalHeader>Wild Pokemon Found!</ModalHeader>
@@ -14,5 +95,3 @@ function CatchingModal(props) {
     </Modal>
   )
 }
-
-export default CatchingModal;
---
+++ src/context/context.js
@@ -0,0 +1,48 @@
+import React, { createContext, useContext, useState } from 'react';
+
+// still don't know what react context is, this comment will be removed when i understand about context.
+export const MyPokemonContext = createContext();
+
+const MyPokemonContextProvider = (props) => {
+  let initialState = [];
+  if(localStorage.hasOwnProperty("myPokemon")) {
+    initialState = JSON.parse(localStorage.getItem("myPokemon"));
+  }
+
+  const [myPokemonList, setMyPokemonList] = useState(initialState);
+
+  return (
+    <MyPokemonContext.Provider value={{
+      myPokemonList,
+      setMyPokemonList
+    }}>
+      {props.children}
+    </MyPokemonContext.Provider>
+  )
+}
+
+export const useMyPokemonList = () => {
+  const {myPokemonList} = useContext(MyPokemonContext);
+  return myPokemonList;
+}
+
+export const useAddMyPokemonList = () => {
+  const {setMyPokemonList} = useContext(MyPokemonContext);
+  return (pokemon) => {
+    setMyPokemonList((prev) => [...prev, pokemon]);
+  };
+}
+
+export const useRemoveMyPokemonList = () => {
+  const {setMyPokemonList} = useContext(MyPokemonContext);
+  return (nickName) => {
+    setMyPokemonList((list) => list.filter((item) => item.nickName !== nickName));
+  };
+}
+
+export const useCountMyPokemon = (name) => {
+  const {myPokemonList} = useContext(MyPokemonContext);
+  return myPokemonList.filter(x => x.name === name).length;
+}
+
+export default MyPokemonContextProvider;
