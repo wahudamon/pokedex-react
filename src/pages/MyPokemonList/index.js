99cd0c920efebe49553c0ba31c742a25e03e0711
wahudamon <afwa.bagas.1999@gmail.com>
2021-09-10 15:18:15 +0700

    feat: sync my pokemon card to my pokemon data in localStorage

--- src/pages/MyPokemonList/index.js
+++ src/pages/MyPokemonList/index.js
@@ -1,11 +1,41 @@
 import '../../App.css';
 import { 
-  Card, CardText, CardBody,
+  Card, 
+  CardText, 
+  CardBody,
   CardTitle,
+  CardImg,
   Row,
   Col,
 } from 'reactstrap';
 import { PokemonColors } from '../../components/PokemonColors';
+import { GetMyPokemonList, MyPokemonProvider } from '../../context/MyContext';
+
+// Soon di pisah component buat pokemon card nya
+
+const MyPokemonCard = () => {
+  const myPokemonList = GetMyPokemonList();
+
+  return (
+    <Row className="mx-5 my-3 g-3">
+      {myPokemonList.map(pokemon => (
+        <Col sm="3" md="5" lg="3">
+          <Card body className="text-center" style={{backgroundColor: pokemon.type ? PokemonColors[pokemon.type[0]] : PokemonColors.normal}}>
+            <CardBody>
+              <CardTitle className="text-capitalize" style={{color: '#000', fontSize: '16pt', fontWeight: 'bold'}}>{pokemon.nickname}</CardTitle>
+              <CardText style={{color: '#000', fontSize: '12pt'}}>({pokemon.name})</CardText>
+              <CardImg width="100%" src={pokemon.image} alt="Card image cap" />
+            </CardBody>
+            <CardBody style={{backgroundColor: '#FFFFFF', borderRadius: '15px'}}>
+              <CardText style={{color: '#000', fontSize: '12pt'}}>Release</CardText>
+            </CardBody>
+          </Card>
+        </Col>
+      ))}
+    </Row>
+  )
+}
+
 
 export default function MyPokemonList() {
   return(
@@ -13,30 +43,9 @@ export default function MyPokemonList() {
       <header className="app-header">
         My Pokemon
       </header>
-      <Row className="mx-5 my-3 g-3">
-        <Col sm="3" md="5" lg="3">
-          <Card body className="text-center" style={{backgroundColor: PokemonColors.fire}}>
-            <CardBody>
-              <CardTitle className="text-capitalize" style={{color: '#FFF', fontSize: '16pt', fontWeight: 'bold'}}>Cimon</CardTitle>
-              <CardText style={{color: '#FFF', fontSize: '12pt'}}>(Charmander)</CardText>
-            </CardBody>
-            <CardBody style={{backgroundColor: '#FFFFFF', borderRadius: '15px'}}>
-              <CardText style={{color: '#000', fontSize: '12pt'}}>Release</CardText>
-            </CardBody>
-          </Card>
-        </Col>
-        <Col sm="3" md="5" lg="3">
-          <Card body className="text-center" style={{backgroundColor: PokemonColors.grass}}>
-            <CardBody>
-              <CardTitle className="text-capitalize" style={{color: '#FFF', fontSize: '16pt', fontWeight: 'bold'}}>Cihuy</CardTitle>
-              <CardText style={{color: '#FFF', fontSize: '12pt'}}>(Bulbasaur)</CardText>
-            </CardBody>
-            <CardBody style={{backgroundColor: '#FFFFFF', borderRadius: '15px'}}>
-              <CardText style={{color: '#000', fontSize: '12pt'}}>Release</CardText>
-            </CardBody>
-          </Card>
-        </Col>
-      </Row>
+        <MyPokemonProvider>
+          <MyPokemonCard/>
+        </MyPokemonProvider>
     </div>
   )
 }
