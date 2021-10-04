1815c5b63b833c1df9b78839bf1aa121c13b7d95
wahudamon <afwa.bagas.1999@gmail.com>
2021-10-04 09:11:16 +0700

    fix: remove unused code at navbar component

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,33 +1,16 @@
-// import { useState } from 'react';
 import {
-  //Collapse,
   Navbar,
-  //NavbarToggler,
   NavbarBrand,
-  //Nav,
-  //NavItem,
-  // NavLink,
 } from 'reactstrap';
 import PokedexLogo from '../../assets/images/pokedex-logo.png';
 
 import '../../App.css';
 
 export default function NavBar() {
-  // const [isOpen, setIsOpen] = useState(false);
-  // const toggle = () => setIsOpen(!isOpen);
-
   return (
     <div>
       <Navbar color="#fff" light expand="md">
         <NavbarBrand className="mx-4 mb-2" href="/"><img src={PokedexLogo} alt="pokedex logo" /></NavbarBrand>
-        {/* <NavbarToggler onClick={toggle} />
-        <Collapse isOpen={isOpen} navbar>
-          <Nav className="mr-auto" navbar>
-            <NavItem>
-              <NavLink href="/my-pokemon">My Pokemon</NavLink>
-            </NavItem>
-          </Nav>
-        </Collapse> */}
       </Navbar>
     </div>
   )
